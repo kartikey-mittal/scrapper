e5ebe78251fc9ae2dfeac873239f592260483df9
jain-lakshay <lakshayjain.code@gmail.com>
2024-07-14 00:09:43 +0530

    dont know

--- src/users/Components/R4/R4.js
+++ src/users/Components/R4/R4.js
@@ -5,13 +5,13 @@ import ScrapPatner from '../ScrapPatner/ScrapPatner';
 const R4 = () => {
   const scrapPartnerData = [
    { imageUrl: 'https://imgs.search.brave.com/yw58VUtQWv3NxZ5x_gBv79ysbdvzGgfDIjl19dvm0wM/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTU1/MzgxNDI5L3Bob3Rv/L3NjcmFwLW1ldGFs/LWNvbG9yLWltYWdl/LmpwZz9zPTYxMng2/MTImdz0wJms9MjAm/Yz1XQzZWWFdYY2Yx/VkJ5LVhoMUlHbnk0/NlVYLUlXZ09TMGJz/S1JYcGRfck1rPQ',partnerInfo: { name: 'Avaril saxena', material: 'Aluminium',weightPerKg:'12' } },
-   { imageUrl: 'https://imgs.search.brave.com/NPql5XtZvmAas8vwf4qtraxoKgOm96yzqCvtAUM52fo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxMi8x/Mi8yNC8wOC8zOC9j/b3BwZXItNzIwNjJf/NjQwLmpwZw',partnerInfo: { name: 'kartikey mittal', material: 'copper',weightPerKg:'10' } },
+   { imageUrl: 'https://imgs.search.brave.com/NPql5XtZvmAas8vwf4qtraxoKgOm96yzqCvtAUM52fo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxMi8x/Mi8yNC8wOC8zOC9j/b3BwZXItNzIwNjJf/NjQwLmpwZw',partnerInfo: { name: 'kartikey mittal', material: 'iron',weightPerKg:'10' } },
    
    { imageUrl: 'https://imgs.search.brave.com/jFMuep15lksqeqwOdIwXEvtjBTc_vewXH_v2ho_YtR0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMz/OTkzNjc4L3Bob3Rv/L2Nsb3NlLXVwLXBo/b3RvLW9mLXNpbHZl/ci1jb2xvci1zY3Jh/cC1tZXRhbC1hbmQt/bWV0YWwtdHJhc2gu/anBnP3M9NjEyeDYx/MiZ3PTAmaz0yMCZj/PW9Wa05CdFZNNGdD/VGpOMldsOWw1M2x3/ZDJ1NnVyNDBBOWYy/TGFFSE54MGM9',partnerInfo: { name: 'lakshay jain', material: 'silver',weightPerKg:'100000' } },
    { imageUrl: 'https://imgs.search.brave.com/jFMuep15lksqeqwOdIwXEvtjBTc_vewXH_v2ho_YtR0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMz/OTkzNjc4L3Bob3Rv/L2Nsb3NlLXVwLXBo/b3RvLW9mLXNpbHZl/ci1jb2xvci1zY3Jh/cC1tZXRhbC1hbmQt/bWV0YWwtdHJhc2gu/anBnP3M9NjEyeDYx/MiZ3PTAmaz0yMCZj/PW9Wa05CdFZNNGdD/VGpOMldsOWw1M2x3/ZDJ1NnVyNDBBOWYy/TGFFSE54MGM9',partnerInfo: { name: 'lakshay jain', material: 'silver',weightPerKg:'100000' } },
   ];
   return (
-   <div style={ {display:'flex',flexDirection:'column', alignContent:'center' ,height:'100%',}}>
+   <div style={ {display:'flex',flexDirection:'column', alignContent:'center' ,height:'100%',padding:'10px'}}>
      <div className="scrollable-container">
       <h4 style={{ color: 'black', paddingLeft: '8px', fontSize: 'large', fontWeight: 'bold' }}>
         Select Scrap Partner
